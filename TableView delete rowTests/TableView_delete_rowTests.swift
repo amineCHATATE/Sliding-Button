00584112b8f14e5bdf7f0ce41a02820763f0a92a
amineCHATATE <amine.chatate@gmail.com>
2019-12-06 17:09:15 +0100

    Initial Commit

---
+++ TableView delete rowTests/TableView_delete_rowTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableView_delete_rowTests.swift
+//  TableView delete rowTests
+//
+//  Created by Amine CHATATE on 06/12/2019.
+//  Copyright © 2019 Amine CHATATE. All rights reserved.
+//
+
+import XCTest
+@testable import TableView_delete_row
+
+class TableView_delete_rowTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
